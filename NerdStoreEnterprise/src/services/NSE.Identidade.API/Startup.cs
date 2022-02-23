811ad0b80069d949a2f8be17785eed1a1fe48cbf
Anderson <anderson@prodwin.com.br>
2022-02-23 17:31:04 -0300

    Implementado as configurações Iniciais do JWT
    Implementado as configurações Iniciais do JWT

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -5,8 +6,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+using System.Text;
 
 namespace NSE.Identidade.API {
 	public class Startup {
@@ -26,26 +30,53 @@ namespace NSE.Identidade.API {
 				.AddEntityFrameworkStores<ApplicationDbContext>()
 				.AddDefaultTokenProviders();
 
+
+			// JWT
+
+			var appSettingsSection = Configuration.GetSection("AppSettings");
+			services.Configure<AppSettings>(appSettingsSection);
+
+			var appSettings = appSettingsSection.Get<AppSettings>();
+			var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+			services.AddAuthentication(options => {
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(bearerOptions => {
+
+				bearerOptions.RequireHttpsMetadata = true;
+				bearerOptions.SaveToken = true;
+				bearerOptions.TokenValidationParameters = new TokenValidationParameters {
+
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidAudience = appSettings.ValidoEm,
+					ValidIssuer = appSettings.Emissor
+				};
+			});
+
 			services.AddSwaggerGen(c => {
 				c.SwaggerDoc("v1", new OpenApiInfo {
 
 					Title = "NerdStore Enterprise Identity API",
 					Description = "Esta API faz parte do curso de ASP.NET Core Enterprise Applications.",
-					Contact = new OpenApiContact() {  Name = "Anderson", Email = "anderson.silvait@outlook.com"},
-					License = new OpenApiLicense() {  Name = "MIT", Url = new System.Uri("https://opensource.org/licenses/MIT")}
-				}); 
+					Contact = new OpenApiContact() { Name = "Anderson", Email = "anderson.silvait@outlook.com" },
+					License = new OpenApiLicense() { Name = "MIT", Url = new System.Uri("https://opensource.org/licenses/MIT") }
+				});
 			});
 
 			services.AddControllers();
 		}
-		
+
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 
 			app.UseSwagger();
-			app.UseSwaggerUI( c=> {
+			app.UseSwaggerUI(c => {
 				c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
 			});
-			
+
 			if(env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			}
