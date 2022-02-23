887c08cd082859c82503cbd2ad3b63eae467db54
Anderson <anderson@prodwin.com.br>
2022-02-23 15:02:15 -0300

    Implementado a configuração inicial do Identity
    Implementado a configuração inicial do Identity

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API {
 	public class Startup {
@@ -19,12 +15,20 @@ namespace NSE.Identidade.API {
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
-			services.AddControllers();
-		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddDefaultIdentity<IdentityUser>()
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
+		
+			services.AddControllers();
+
+		}
+		
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if(env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
@@ -35,6 +39,8 @@ namespace NSE.Identidade.API {
 			app.UseRouting();
 
 			app.UseAuthorization();
+
+			app.UseAuthentication();
 
 			app.UseEndpoints(endpoints => {
 				endpoints.MapControllers();
